3e36acab82be5253705f4ebdcda652836bfb371c
nwwb-lvweichen <nwwb-lvweichen@nuanwa.net>
2022-04-21 15:19:43 +0800

    最长无重复字符串

---
+++ src/main/java/lv/aaa/likou/StringNotRepeatMax.java
@@ -0,0 +1,48 @@
+package lv.aaa.likou;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+/**
+ * @author lvweichen 🛺 ☁ ☁ ☁ …… ️🏃
+ * @date 2022/4/21 下午3:14
+ */
+public class StringNotRepeatMax {
+
+    public static void main(String[] args) {
+        String s = "abcabcbb";
+        lengthOfLongestSubstring(s);
+    }
+    public static int lengthOfLongestSubstring(String s) {
+        String[] targetChars = s.split("");
+        if (targetChars.length <= 1) {
+            return "".equals(s) ? 0 : 1;
+        }
+        int index = 0;
+        List<Integer> nums = new ArrayList();
+        int i = 0;
+        Map<String,Integer> hash = new HashMap();
+        while(i < targetChars.length){
+            if(hash.containsKey(targetChars[i])){
+                nums.add(i - index);
+                index++;
+                hash.clear();
+                i = index;
+                continue;
+            }else{
+                if(i == targetChars.length - 1){
+                    if(!hash.containsKey(targetChars[i])){
+                        nums.add(targetChars.length - index);
+                    }
+                }
+            }
+            hash.put(targetChars[i],1);
+            i++;
+        }
+        List<Integer> collect = nums.stream().sorted().collect(Collectors.toList());
+        return collect.size() == 0 ? hash.size() : collect.get(collect.size() - 1);
+    }
+}
