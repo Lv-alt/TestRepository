b23398f54cf564158d4e79c8f20f8682040d9cfa
nwwb-lvweichen <nwwb-lvweichen@nuanwa.net>
2022-06-21 18:03:31 +0800

    编程式事物

--- src/main/java/lv/aaa/util/BinlogConsumer.java
+++ src/main/java/lv/aaa/util/BinlogConsumer.java
@@ -5,12 +5,21 @@ import com.github.shyiko.mysql.binlog.event.DeleteRowsEventData;
 import com.github.shyiko.mysql.binlog.event.EventData;
 import com.github.shyiko.mysql.binlog.event.UpdateRowsEventData;
 import com.github.shyiko.mysql.binlog.event.WriteRowsEventData;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.support.TransactionCallback;
+import org.springframework.transaction.support.TransactionTemplate;
+
+import java.util.function.Function;
 
 /**
  * @author lvweichen 🛺 ☁ ☁ ☁ …… ️🏃
  * @date 2022/6/21 下午4:42
  */
 public class BinlogConsumer {
+    
+    @Autowired
+    private TransactionTemplate transactionTemplate;  
+    
     public static void main(String[] args) throws Exception {
         BinaryLogClient logClient = new BinaryLogClient(
                 "127.0.0.1", 3306, "test", "root", "1234"
@@ -35,5 +44,15 @@ public class BinlogConsumer {
 
         // 开始监听
         logClient.connect();
+    }
+
+    /**
+     * 编程式事物
+     * @param action
+     * @param <T>
+     * @return
+     */
+    public <T> T a(TransactionCallback<T> action){
+        return transactionTemplate.execute(action);
     }
 }
